e38675c2306cdc889193869b4ee30587469da6e3
Haroldo Júnio <haroldojunios@gmail.com>
2021-12-09 09:33:40 -0300

    Setup: conexão com mongo e esquema/modelo

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
+
+const articleSchema = { title: String, content: String };
+
+const Article = mongoose.model("Article", articleSchema);
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
